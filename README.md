59042c43f18f7d43a91a25309f8f97d00a446ccb
robinski <robinskilling@gmail.com>
2023-11-06 18:19:02 +0000

    refactor as a class method

---
+++ README.md
@@ -0,0 +1,21 @@
+########
+
+Setup
+=======
+install ruby
+
+install bundler
+
+`bundle install`
+
+`irb` to enter ruby console
+
+`require './validate_password'`
+
+`ValidatePassword.valid?("yourpassword")`
+
+
+Tests
+======
+
+`ruby validate_password_spec.rb`
--- validate_password.rb
+++ validate_password.rb
@@ -1,37 +1,36 @@
 class ValidatePassword
-  def initialize password
-    @password = password.to_s
-  end
+  class << self
+    def valid?(password)
+      @password = password.to_s
+      return false unless min_length?
+      return false unless contains_capital_letter?
+      return false unless lower_case_letter?
+      return false unless underscore?
+      return false unless number?
 
-  def valid?
-    return false unless min_length?
-    return false unless contains_capital_letter?
-    return false unless lower_case_letter?
-    return false unless underscore?
-    return false unless number?
+      true
+    end
 
-    true
-  end
+    private
 
-  private
+    def min_length?
+      @password.length >= 8
+    end
 
-  def min_length?
-    @password.length >= 8
-  end
+    def contains_capital_letter?
+      @password.match(/[A-Z]/)
+    end
 
-  def contains_capital_letter?
-    @password.match(/[A-Z]/)
-  end
+    def lower_case_letter?
+      @password.match(/[a-z]/)
+    end
 
-  def lower_case_letter?
-    @password.match(/[a-z]/)
-  end
+    def underscore?
+      @password.match(/[_]/)
+    end
 
-  def underscore?
-    @password.match(/[_]/)
-  end
-
-  def number?
-    @password.match(/[0-9]/)
+    def number?
+      @password.match(/[0-9]/)
+    end
   end
 end
